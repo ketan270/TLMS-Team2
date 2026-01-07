1c127098d996a80fb17926a0edee9b36bff78c92
ketan270 <ketansharma2005@gmail.com>
2026-01-07 14:40:29 +0530

    struct: Added project structure of TLMS

---
+++ TLMS-Team2/TLMS-Team2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TLMS-Team2
+//
+//  Created by Ketan Sharma on 07/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
